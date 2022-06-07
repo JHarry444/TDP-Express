ceca0268c644e04ef6671f848d15ce1bcf6307c4
jordan <jharrison1@qa.com>
2022-06-07 14:28:19 +0100

    added eslint and nodemon

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-const express = require("express"); // importing express
+const express = require('express'); // importing express
 
 const app = express(); // initialising it
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json()); // converts the request body from JSON (res => res.json())
 
-app.get("/hello", (req, res) => {
-    res.send("Hello, World!");
+app.get('/greeting', (req, res) => {
+  res.send('Hello, World!');
 });
 
-app.post("/create", (req, res) => {
-    console.log("Received:", req.body);
-    res.status(201).send("Created successfully");
+app.post('/create', (req, res) => {
+  console.log('Received:', req.body);
+  res.status(201).send('Created successfully');
 });
 
-app.put("/update/:id", (req, res) => {
-    console.log("Query parameters:", req.query);
-    res.send();
+app.put('/update/:id', (req, res) => {
+  console.log('Query parameters:', req.query);
+  res.send();
 });
 
-app.delete("/remove/:id", (req, res) => {
-    console.log("Params:", req.params);
-    res.status(204).send();
-})
+app.delete('/remove/:id', (req, res) => {
+  console.log('Params:', req.params);
+  res.status(204).send();
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status).send(err.message);
+});
 
 const server = app.listen(4494, () => {
-    console.log("Server started on", server.address().port);
+  console.log('Server started on', server.address().port);
 });
