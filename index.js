cc11a06aa250690fb679be3ef41cc6f2eec26933
jordan <jharrison1@qa.com>
2022-06-07 11:02:35 +0100

    added request handling

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require("express"); // importing express
 
 const app = express(); // initialising it
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json()); // converts the request body from JSON (res => res.json())
+
+app.get("/hello", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.post("/create", (req, res) => {
+    console.log("Received:", req.body);
+    res.status(201).send("Created successfully");
+});
+
+app.put("/update/:id", (req, res) => {
+    console.log("Query parameters:", req.query);
+    res.send();
+});
+
+app.delete("/remove/:id", (req, res) => {
+    console.log("Params:", req.params);
+    res.status(204).send();
+})
 
 const server = app.listen(4494, () => {
     console.log("Server started on", server.address().port);
