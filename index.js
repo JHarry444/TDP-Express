9060425afe484efbb0d631a6459479aed7abb070
jordan <jharrison1@qa.com>
2022-06-07 16:24:26 +0100

    updated example

--- index.js
+++ index.js
@@ -8,33 +8,54 @@ app.use(cors());
 app.use(bodyParser.json()); // converts the request body from JSON (res => res.json())
 
 app.use((req, res, next) => {
-  console.log('Request received at', new Date());
-  return next();
+  const logEntry = `host: ${req.host}
+    ip: ${req.ip}
+    method: ${req.method}
+    path: ${req.path}
+    time: ${new Date()}`;
+  console.log(logEntry);
+  next();
 });
+
+const names = ['Jordan', 'Cameron', 'Chris', 'Aswene'];
 
 app.get('/greeting', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/readAll', (req, res) => res.send(names));
+
+app.get('/read/:id', (req, res, next) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id) || id < 0 || id >= names.length) return next({ status: 400, message: 'Invalid id' });
+  return res.send(names[id]);
+});
+
 app.post('/create', (req, res) => {
-  console.log('Received:', req.body);
-  res.status(201).send('Created successfully');
+  const newName = req.body.name;
+  names.push(newName);
+  res.status(201).send(`Successfully added ${names[names.length - 1]}`);
 });
 
 app.put('/update/:id', (req, res, next) => {
-  const id = parseInt(req.params.id);
-  if (!id) return next({ status: 400, message: 'Invalid id' });
-  console.log('Query parameters:', req.query);
-  return res.send();
+  const id = parseInt(req.params.id, 10);
+  const newName = req.query.name;
+  if (Number.isNaN(id) || id < 0 || id >= names.length) return next({ status: 400, message: 'Invalid id' });
+  const oldName = names[id];
+  names.splice(id, 1, newName);
+  return res.send(`Replaced ${oldName} with ${names[id]}`);
 });
 
-app.delete('/remove/:id', (req, res) => {
-  console.log('Params:', req.params);
-  res.status(204).send();
+app.delete('/remove/:id', (req, res, next) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id) || id < 0 || id >= names.length) return next({ status: 400, message: 'Invalid id' });
+  names.splice(id, 1);
+  return res.status(204).send();
 });
 
 app.use('*', (req, res, next) => next({ status: 404, message: 'Invalid url' })); // catches 404's
 
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   res.status(err.status).send(err.message);
 });
