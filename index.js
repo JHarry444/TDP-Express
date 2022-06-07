c2012d6bfcdf3c1659f57490c91e4fb682a0966b
jordan <jharrison1@qa.com>
2022-06-07 16:04:30 +0100

    added error handling and middleware examples

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express'); // importing express
 
 const app = express(); // initialising it
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json()); // converts the request body from JSON (res => res.json())
+
+app.use((req, res, next) => {
+  console.log('Request received at', new Date());
+  return next();
+});
 
 app.get('/greeting', (req, res) => {
   res.send('Hello, World!');
@@ -14,15 +21,19 @@ app.post('/create', (req, res) => {
   res.status(201).send('Created successfully');
 });
 
-app.put('/update/:id', (req, res) => {
+app.put('/update/:id', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  if (!id) return next({ status: 400, message: 'Invalid id' });
   console.log('Query parameters:', req.query);
-  res.send();
+  return res.send();
 });
 
 app.delete('/remove/:id', (req, res) => {
   console.log('Params:', req.params);
   res.status(204).send();
 });
+
+app.use('*', (req, res, next) => next({ status: 404, message: 'Invalid url' })); // catches 404's
 
 app.use((err, req, res, next) => {
   res.status(err.status).send(err.message);
