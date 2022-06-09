fdb057e66b9708dc1ec23182557d34471b2e4e04
jordan <jharrison1@qa.com>
2022-06-09 11:02:34 +0100

    swapped expected and actual

--- tests/test.js
+++ tests/test.js
@@ -4,12 +4,12 @@ const maths = require('./maths');
 
 mocha.describe('Test adding', () => {
   mocha.it('should equal 2', () => {
-    chai.expect(maths.add(1, 1)).to.equal(2);
+    chai.expect(2).to.equal(maths.add(1, 1));
   });
   mocha.it('should equal 4', () => {
-    chai.expect(maths.add(2, 2)).to.equal(4);
+    chai.expect(4).to.equal(maths.add(2, 2));
   });
   mocha.it('should equal 10', () => {
-    chai.expect(maths.add(5, 6)).to.equal(10);
+    chai.expect(10).to.equal(maths.add(5, 6));
   });
 });
