b1acb710521ed65fb22478757f47d1b03d3f9eaf
jordan <jharrison1@qa.com>
2022-06-10 09:36:18 +0100

    added reverse factorial example

--- tests/maths.js
+++ tests/maths.js
@@ -3,6 +3,17 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+const reverseFactorial = (num) => {
+  let i = 1;
+  let n = num;
+  while (n > 1) {
+    i += 1;
+    n /= i;
+  }
+  if (n === 1) return `${i}!`;
+  return 'NONE';
+};
+
 module.exports = {
-  add, subtract, multiply, divide,
+  add, subtract, multiply, divide, reverseFactorial,
 };
--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,27 @@ const mocha = require('mocha');
 const chai = require('chai');
 const maths = require('./maths');
 
+mocha.describe('Reverse factorial', () => {
+  mocha.it('should return 1!', () => {
+    chai.expect('1!').to.equal(maths.reverseFactorial(1));
+  });
+  mocha.it('should return 2!', () => {
+    chai.expect('2!').to.equal(maths.reverseFactorial(2));
+  });
+  mocha.it('should return 3!', () => {
+    chai.expect('3!').to.equal(maths.reverseFactorial(6));
+  });
+  mocha.it('should return 4!', () => {
+    chai.expect('4!').to.equal(maths.reverseFactorial(24));
+  });
+  mocha.it('should return 5!', () => {
+    chai.expect('5!').to.equal(maths.reverseFactorial(120));
+  });
+  mocha.it('should return NONE', () => {
+    chai.expect('NONE').to.equal(maths.reverseFactorial(130));
+  });
+});
+
 mocha.describe('Test adding', () => {
   mocha.it('should equal 2', () => {
     chai.expect(2).to.equal(maths.add(1, 1));
